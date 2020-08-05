8c3d38a4704d69f1eb3d7eafa0c18d397377623a
Arina <hadson.rey@mail.ru>
2020-08-05 15:00:14 +1100

    Стартовая сборка

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+const rename = require("gulp-rename");
+const del = require("del");
+
+const gulpStylelint = require("gulp-stylelint");
+const inject = require("gulp-inject");
+const server = require("browser-sync").create();
+
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+
+sass.compiler = require("node-sass");
+
+const stylelint = function () {
+    return gulp.src("source/styles/**/*.scss")
+        .pipe(gulpStylelint({
+            reporters: [
+                {formatter: 'string', console: true}
+            ],
+            failAfterError: true
+        }))
+};
+
+const clean = function () {
+    return del("build");
+};
+
+const reloadServer = function (cb) {
+    server.reload();
+    cb();
+};
+
+const runServer = function () {
+    server.init({
+        server: "build/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch("source/styles/**/*.{scss,sass}", gulp.series(processSass, reloadServer));
+    gulp.watch("source/*.html", gulp.series(processHtml, reloadServer));
+};
+
+const processSass = function () {
+    return gulp.src("source/styles/main.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(csso())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"));
+};
+
+const processHtml = function () {
+    const sources = gulp.src(["build/js/*.js", "build/css/*.css"], {read: false, base: "build/"});
+    return gulp.src("source/index.html")
+        .pipe(inject(sources, {
+            transform: function (filepath, file) {
+                const fileName = filepath.replace('/' + file.base + '/', '');
+                return inject.transform.call(inject.transform, fileName);
+            }
+        }))
+        .pipe(gulp.dest("build"))
+};
+
+const build = gulp.series(clean, processSass, processHtml);
+
+exports.build = build;
+exports.start = gulp.series(build, runServer);
+exports.autolint = stylelint;
