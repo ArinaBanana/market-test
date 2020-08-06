4ee22a4de3fbb5a6ad5e1e35a9f42251067338c8
Arina <hadson.rey@mail.ru>
2020-08-07 02:23:38 +1100

    Рефакторинг

--- source/component/gallery-item/GalleryItem.vue
+++ source/component/gallery-item/GalleryItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="gallery__item">
-    <img class="gallery__img" src="https://picsum.photos/200/200.webp" alt="Product item">
+    <img class="gallery__img" :src="path" alt="Product item">
   </div>
 </template>
 
